942ad4a451a5394d0f0f219cb28005967dc2c5e3
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2023-12-04 19:51:48 +0100

    Solve part 2 of day 4

--- 4/main.rs
+++ 4/main.rs
@@ -1,6 +1,11 @@
 const INPUT: &str = include_str!("input");
 
 fn main() {
+    part1();
+    part2();
+}
+
+fn part1() {
     let part1: u32 = INPUT
         .lines()
         .map(|line| {
@@ -29,4 +34,45 @@ fn main() {
         .sum();
 
     println!("{}", part1);
+}
+
+fn part2() {
+    let num_winning_numbers: Vec<usize> = INPUT
+        .lines()
+        .map(|line| {
+            let integers: [usize; 36] = line
+                .split_whitespace()
+                .map(|entry| entry.trim_end_matches(':'))
+                .filter_map(|number| usize::from_str_radix(number, 10).ok())
+                .collect::<Vec<usize>>()
+                .try_into()
+                .unwrap();
+
+            let my_numbers = &integers[1..11];
+            let winning_numbers = &integers[11..36];
+
+            let mut count = 0;
+            for winning_number in winning_numbers {
+                if my_numbers.contains(winning_number) {
+                    count += 1;
+                }
+            }
+
+            count
+        })
+        .collect();
+
+    assert_eq!(num_winning_numbers.len(), 209);
+
+    let mut cards = [1; 209];
+
+    let mut part2 = 0;
+    for id in 0..209 {
+        for incr_idx in (id + 1)..(id + num_winning_numbers[id] + 1) {
+            cards[incr_idx] += cards[id];
+        }
+        part2 += cards[id];
+    }
+
+    println!("{}", part2);
 }
