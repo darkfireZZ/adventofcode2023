ca2ec6961214110ae10c1e683936701bc2141a95
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2023-12-10 18:14:00 +0100

    Solve part 2 of day 9

--- 9/main.rs
+++ 9/main.rs
@@ -1,28 +1,44 @@
 const EXAMPLE_INPUT: &str = include_str!("example_input");
-const EXAMPLE_SOLUTION: i64 = 114;
+const EXAMPLE_SOLUTION_1: i64 = 114;
+const EXAMPLE_SOLUTION_2: i64 = 2;
 const INPUT: &str = include_str!("input");
 
 fn main() {
-    assert_eq!(part1(EXAMPLE_INPUT), EXAMPLE_SOLUTION);
+    assert_eq!(part1(EXAMPLE_INPUT), EXAMPLE_SOLUTION_1);
 
     let p1_sol = part1(INPUT);
     println!("part 1: {}", p1_sol);
+
+    assert_eq!(part2(EXAMPLE_INPUT), EXAMPLE_SOLUTION_2);
+
+    let p2_sol = part2(INPUT);
+    println!("part 2: {}", p2_sol);
 }
 
 fn part1(input: &str) -> i64 {
-    input
-        .lines()
-        .map(|line| {
-            let line = line
-                .split_whitespace()
-                .map(|number| i64::from_str_radix(number, 10).unwrap())
-                .collect();
-            predict_value(line)
+    parse_lines(input)
+        .map(|line| predict_value_right(line))
+        .sum()
+}
+
+fn part2(input: &str) -> i64 {
+    parse_lines(input)
+        .map(|mut line| {
+            line.reverse();
+            predict_value_right(line)
         })
         .sum()
 }
 
-fn predict_value(mut line: Vec<i64>) -> i64 {
+fn parse_lines(input: &str) -> impl Iterator<Item = Vec<i64>> + '_ {
+    input.lines().map(|line| {
+        line.split_whitespace()
+            .map(|number| i64::from_str_radix(number, 10).unwrap())
+            .collect()
+    })
+}
+
+fn predict_value_right(mut line: Vec<i64>) -> i64 {
     let num_values = line.len();
 
     let mut count = num_values;
